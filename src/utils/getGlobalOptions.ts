805b596b02aad048a5758590ef360ce08335f2db
Gago <xeroice@gmail.com>
2020-05-15 14:30:30 -0700

    making Parameters have always a prefix defined & workflow

--- src/utils/getGlobalOptions.ts
+++ src/utils/getGlobalOptions.ts
@@ -2,7 +2,7 @@ import { CustomConfig, getCustomConfiguration } from './getCustomConfiguration';
 
 interface Options { prefix?: string, awsProfile?: string, environment?: string, awsRegion?: string, awsAccessKeyId?: string, awsSecretAccessKey?: string, awsSessionToken?: string }
 type PossibleCredentials = { profile?: string, accessKeyId?: string, secretAccessKey?: string, sessionToken?: string };
-type Parameters = { prefix?: string, region?: string, environment?: string }
+type Parameters = { prefix: string, region?: string, environment?: string }
 export interface Command { parent: Options }
 export const getGlobalOptions = async (command: Command): Promise<{ params: Parameters, credentials: PossibleCredentials }> => {
   let customConfiguration: CustomConfig | void
@@ -19,17 +19,26 @@ export const getGlobalOptions = async (command: Command): Promise<{ params: Para
     }
   } = command
 
+  const credentials = { profile, accessKeyId, secretAccessKey, sessionToken };
+  let params: Parameters;
+
   if (!prefix) {
     customConfiguration = await getCustomConfiguration();
 
-    if (!customConfiguration) {
+    if (customConfiguration && 'prefix' in customConfiguration && typeof customConfiguration.prefix === 'string') {
+      const { prefix: customPrefix, ...rest } = customConfiguration;
+      params = { prefix: customPrefix, ...rest };
+    } else {
       console.error('prefix not provided, try again with --prefix option');
       process.exit(1);
     }
+  } else {
+    params = { prefix, environment, region }
   }
 
   return {
-    credentials: { profile, accessKeyId, secretAccessKey, sessionToken },
-    params: { prefix, region, environment, ...customConfiguration }
+    credentials,
+    params
   }
+
 }
