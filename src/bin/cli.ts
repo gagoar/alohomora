7944afe5024e973389dc1832794b4290fdcd72e3
Gago <xeroice@gmail.com>
2020-05-14 19:19:24 -0700

    some <> sbould just be []

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -8,12 +8,12 @@ program
   .version(packageJSON.version)
   .description(packageJSON.description)
   .requiredOption('-p, --prefix <prefix>', 'The prefix used to store the key (it should not start or end with a `/`, ex: if the path to the secret is `/my-app/[env]/secretName`, the prefix will be `my-app` )')
-  .option('--aws-region <region>', 'The AWS region code where the secrets have been stored (default us-east-1), for more info https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html#concepts-available-regions')
-  .option(`--environment <environment>', 'The environment for the secrets, the available environments are: [${Object.keys(Environment).join("|")}]`)
-  .option('--aws-access-key-id <AWS_ACCESS_KEY_ID>', 'Following https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-environment.html')
-  .option('--aws-secret-access-key <AWS_SECRET_ACCESS_KEY>', 'Following https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-environment.html')
-  .option('--aws-session-token <AWS_SESSION_TOKEN>', 'Following https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-environment.html')
-  .option('--aws-profile <AWS_PROFILE>', 'Following https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-shared.html')
+  .option('--aws-region [region]', 'The AWS region code where the secrets have been stored (https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html#concepts-available-regions)', 'us-east-1')
+  .option(`--environment [environment]', 'The environment for the secrets [${Object.keys(Environment).join("|")}]`, 'all')
+  .option('--aws-access-key-id [AWS_ACCESS_KEY_ID]', 'Following https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-environment.html', undefined)
+  .option('--aws-secret-access-key [AWS_SECRET_ACCESS_KEY]', 'Following https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-environment.html', undefined)
+  .option('--aws-session-token [AWS_SESSION_TOKEN]', 'Following https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-environment.html', undefined)
+  .option('--aws-profile [AWS_PROFILE]', 'Following https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-shared.html', undefined)
 
 program
   .command('list')
