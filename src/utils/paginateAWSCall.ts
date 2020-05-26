cb5752f572f376d867a8751629ee6e4d0be0c5b3
Gago <xeroice@gmail.com>
2020-05-25 21:54:42 -0700

    no need of having extend any on TResponse

--- src/utils/paginateAWSCall.ts
+++ src/utils/paginateAWSCall.ts
@@ -7,7 +7,8 @@ type AWSPromise<T> = {
 type AWSParams = Record<string, any> & { NextToken?: string };
 
 type AWSResponse = { Parameters?: any[], NextToken?: string }
-export const paginateAWSCall = async<TParams extends AWSParams, TResult extends AWSResponse, TResponse extends any>
+
+export const paginateAWSCall = async<TParams extends AWSParams, TResult extends AWSResponse, TResponse>
   (params: TParams, getter: (params: TParams) => AWSPromise<TResult>): Promise<TResponse[]> => {
 
   const { NextToken, Parameters: parameters = [] } = await getter(params).promise()
